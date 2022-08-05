c9b903ed639f45984f2faaac36abd50aa076f1f1
debasish-afk <debasishbehera904@gmail.com>
2022-08-05 17:48:15 +0530

    commited route file

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,9 +3,13 @@ const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+<<<<<<< Updated upstream
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+=======
+    res.send('juju')
+>>>>>>> Stashed changes
 });
 
 
