60bb501c4774f548cf55f03b3e3c84604e0f0ff9
debasish-afk <debasishbehera904@gmail.com>
2022-08-12 16:22:27 +0530

    new files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const{default:mongoose}=require('mongoose')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
+mongoose.connect("mongodb+srv://Debasish904:Nzi5BjnfyWQmSY9m@cluster0.eeozuz6.mongodb.net/juju?retryWrites=true&w=majority", 
+{
+    useNewurlParser: true
+}).then(()=>{console.log("mongodb is connected")})
+.catch(err=>console.log(err))
+
+
+app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
